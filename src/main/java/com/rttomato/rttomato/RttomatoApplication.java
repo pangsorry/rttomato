f8a437a2499c09610dd3c081619a8380129992b2
pangsorry <dlathfdmlgkf@naver.com>
2022-04-21 14:06:14 +0900

    first

---
+++ src/main/java/com/rttomato/rttomato/RttomatoApplication.java
@@ -0,0 +1,13 @@
+package com.rttomato.rttomato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RttomatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RttomatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rttomato/rttomato/RttomatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rttomato.rttomato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RttomatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
